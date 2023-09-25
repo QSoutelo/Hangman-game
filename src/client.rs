a62b76019ddf04e9b79a640535f800c5bdc2dd55
Quentin <quentin.soutelo@reseau.eseo.fr>
2023-09-25 10:01:15 +0200

    ajout base client et serveur

---
+++ src/client.rs
@@ -0,0 +1,30 @@
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+use std::thread;
+use std::io::BufRead;
+fn main() -> io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
+    println!("Connected to the server.");
+
+    let mut stream_clone = stream.try_clone()?; // Clone the stream for the input thread
+
+    thread::spawn(move || {
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            let input = line.unwrap();
+            stream_clone.write(input.as_bytes()).unwrap();
+        }
+    });
+
+    let mut buffer = [0; 128];
+    loop {
+        let bytes_read = stream.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        let response = String::from_utf8_lossy(&buffer[0..bytes_read]);
+        print!("{}", response);
+    }
+
+    Ok(())
+}

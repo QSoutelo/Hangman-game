b4e686368c85846f2a7bfb8207cb30d3b0ad88c2
Emilien-Valain <emilien.valain@gmail.com>
2023-10-11 10:56:06 +0200

    ajout du trait joueur

--- serveur/src/main.rs
+++ serveur/src/main.rs
@@ -7,14 +7,46 @@ use std::thread;
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
 
-fn handle_client(mut stream: TcpStream, word_to_guess: String,game_state: Arc<Mutex<GameState>>) {
+// Définition d'un trait Joueur.
+trait Joueur {
+    fn jouer(&mut self, lettre: char) -> bool;
+}
+
+impl Joueur for GameState {
+    fn jouer(&mut self, lettre: char) -> bool {
+        if self.guessed_letters.contains(&lettre) {
+            return false;
+        }
+
+        if self.word_to_guess.contains(lettre) {
+            for (i, letter) in self.word_to_guess.chars().enumerate() {
+                if letter == lettre {
+                    self.current_state[i] = lettre;
+                }
+            }
+            self.guessed_letters.insert(lettre);
+            true
+        } else {
+            self.attempts -= 1;
+            self.guessed_letters.insert(lettre);
+            false
+        }
+    }
+}
+
+struct GameState {
+    word_to_guess: String,
+    current_state: Vec<char>,
+    guessed_letters: HashSet<char>,
+    attempts: i32,
+}
+
+fn handle_client(mut stream: TcpStream, word_to_guess: String, game_state: Arc<Mutex<GameState>>) {
     let mut guessed_letters = HashSet::new();
-    let mut attempts = 7;
     let word_chars: Vec<char> = word_to_guess.chars().collect();
     let word_len = word_to_guess.len();
-    let _current_state = vec!['_'; word_len];
 
-    while attempts > 0 && game_state.lock().unwrap().current_state.contains(&'_') {
+    while game_state.lock().unwrap().attempts > 0 && game_state.lock().unwrap().current_state.contains(&'_') {
         let mut buffer = [0; 1];
         stream.read(&mut buffer).unwrap();
         let guess = buffer[0] as char;
@@ -23,20 +55,15 @@ fn handle_client(mut stream: TcpStream, word_to_guess: String,game_state: Arc<Mu
             stream.write(b"You already guessed this letter!\n").unwrap();
             continue;
         }
+
         let mut locked_game_state = game_state.lock().unwrap();
 
-        if locked_game_state.word_to_guess.contains(guess) {
+        if locked_game_state.jouer(guess) {
             stream.write(b"Correct guess!\n").unwrap();
-            for (i, letter) in word_chars.iter().enumerate() {
-                if letter == &guess {
-                    locked_game_state.current_state[i] = guess;
-                }
-            }
         } else {
             stream.write(b"Wrong guess!\n").unwrap();
-            //depending on how much attempts left print a different message
 
-            match attempts {
+            match locked_game_state.attempts {
                 1 => {stream.write(b"  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========\n").unwrap(); }
                 2 => { stream.write(b"  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========\n").unwrap();}
                 3 => {stream.write(b"  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========\n").unwrap();}
@@ -46,7 +73,7 @@ fn handle_client(mut stream: TcpStream, word_to_guess: String,game_state: Arc<Mu
                 _ => {stream.write(b"  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========\n").unwrap();}
             }
 
-            attempts -= 1;
+            locked_game_state.attempts -= 1;
         }
 
         guessed_letters.insert(guess);
@@ -54,17 +81,13 @@ fn handle_client(mut stream: TcpStream, word_to_guess: String,game_state: Arc<Mu
         stream.write(b"\n").unwrap();
     }
 
-    if attempts > 0 {
+    if game_state.lock().unwrap().attempts > 0 {
         stream.write(b"You won!\n").unwrap();
     } else {
         stream.write(b"You lost! The word was ").unwrap();
         stream.write(word_to_guess.as_bytes()).unwrap();
         stream.write(b"\n").unwrap();
     }
-}
-struct GameState {
-    word_to_guess: String,
-    current_state: Vec<char>,
 }
 
 fn main() {
@@ -76,13 +99,16 @@ fn main() {
     let initial_state = Arc::new(Mutex::new(GameState {
         word_to_guess: word_to_guess.clone(),
         current_state: vec!['_'; word_to_guess.len()],
+        guessed_letters: HashSet::new(),
+        attempts: 7,
     }));
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         let word_to_guess = word_to_guess.clone();
         let game_state = initial_state.clone();
         thread::spawn(move || {
-            handle_client(stream, word_to_guess,game_state);
+            handle_client(stream, word_to_guess, game_state);
         });
     }
 }

0699a2ccb574b129b297d0b7a516dd2f075d9ba9
Quentin <quentin.soutelo@reseau.eseo.fr>
2023-10-11 10:01:33 +0200

    autre com dysfonctionnel

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,15 @@
-use std::io::{self, Read, Write};
+
+use std::io::{self, prelude::*, BufReader, Write};
 use std::net::TcpStream;
+use std::str;
 use std::thread;
-use std::io::BufRead;
+use std::sync::mpsc;
 fn main() -> io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:8080")?;
     println!("Connected to the server.");
 
     let mut stream_clone = stream.try_clone()?; // Clone the stream for the input thread
-
+    let (output_sender, output_receiver) = mpsc::channel();
     thread::spawn(move || {
         let stdin = io::stdin();
         for line in stdin.lock().lines() {
@@ -18,6 +20,16 @@ fn main() -> io::Result<()> {
 
     let mut buffer = [0; 128];
     loop {
+        // Lire les mises à jour du jeu depuis le serveur
+        let mut reader = BufReader::new(&stream);
+        let mut buffer: Vec<u8> = Vec::new();
+        reader.read_until(b'\n', &mut buffer)?;
+        let received_message = format!("Received: {}", str::from_utf8(&buffer).unwrap());
+        output_sender.send(received_message).unwrap();
+       //println!("\n read from server: {}", str::from_utf8(&buffer).unwrap());
+        println!("");
+        
+        // Lire la réponse du serveur
         let bytes_read = stream.read(&mut buffer)?;
         if bytes_read == 0 {
             break;

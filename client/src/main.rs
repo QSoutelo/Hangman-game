84e438edd5495ddc33ef649988a7a82df00e92e6
Emilien-Valain <emilien.valain@gmail.com>
2023-10-17 11:31:26 +0200

    gestion d'erreur client

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,17 +2,24 @@ use std::io::{self, Read, Write};
 use std::net::TcpStream;
 use std::thread;
 use std::io::BufRead;
+
+fn handle_input(mut stream: TcpStream) -> io::Result<()> {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let input = line?;
+        stream.write(input.to_lowercase().as_bytes())?;
+    }
+    Ok(())
+}
+
 fn main() -> io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:8080")?;
     println!("Connected to the server.");
 
-    let mut stream_clone = stream.try_clone()?; // Clone the stream for the input thread
-
-    thread::spawn(move || {
-        let stdin = io::stdin();
-        for line in stdin.lock().lines() {
-            let input = line.unwrap();
-            stream_clone.write(input.to_lowercase().as_bytes()).unwrap();
+    let stream_clone = stream.try_clone()?;
+    let input_thread = thread::spawn(move || {
+        if let Err(err) = handle_input(stream_clone) {
+            eprintln!("Error in input thread: {}", err);
         }
     });
 
@@ -25,6 +32,8 @@ fn main() -> io::Result<()> {
         let response = String::from_utf8_lossy(&buffer[0..bytes_read]);
         print!("{}", response);
     }
+
+    input_thread.join().expect("Input thread panicked.");
 
     Ok(())
 }
